ec0d3e7471749fd338733d7410a77879d3ef744e
Dmitriy <dm.kolotov@mail.ru>
2016-10-22 23:50:55 +0200

    Loop widget done

--- src/vi2.core.annotated-timeline.js
+++ src/vi2.core.annotated-timeline.js
@@ -1,35 +1,35 @@
 /* 
-* name: Vi2.AnnotatedTimline
-* author: niels.seidel@nise81.com
-* license: MIT License
-* description:
-* depends on:
-*  - jquery-1.11.2.min.js
-*  - jquery.inherit-1.1.1.js
-* related code:		
-	- timeline preview: https://github.com/brightcove/videojs-thumbnails/blob/master/videojs.thumbnails.js
+ * name: Vi2.AnnotatedTimline
+ * author: niels.seidel@nise81.com
+ * license: MIT License
+ * description:
+ * depends on:
+ *  - jquery-1.11.2.min.js
+ *  - jquery.inherit-1.1.1.js
+ * related code:
+ - timeline preview: https://github.com/brightcove/videojs-thumbnails/blob/master/videojs.thumbnails.js
 
-* todo:
-*   - cluster / zoom : - Vergleiche Darstellung von sehr vielen Markern bei Google Maps ...
-		- diagramm (?)
-		- filter
-		- multi tracks
-		- add slide preview
-		- bug: previw is not exact and is loading very slow
+ * todo:
+ *   - cluster / zoom : - Vergleiche Darstellung von sehr vielen Markern bei Google Maps ...
+ - diagramm (?)
+ - filter
+ - multi tracks
+ - add slide preview
+ - bug: previw is not exact and is loading very slow
 
-*/
+ */
 
 
 Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ // 
 
   /** @constructs
-  *		@param {object} options An object containing the parameters
-  */
+   *		@param {object} options An object containing the parameters
+   */
   __constructor : function(video, options, seek) {
-      this.video = video;
-      this.options = $.extend(this.options, options);
-      this.seek = seek === undefined ? 0 : seek;
-      this.init();
+    this.video = video;
+    this.options = $.extend(this.options, options);
+    this.seek = seek === undefined ? 0 : seek;
+    this.init();
   },
 
   name : 'annotated timeline',
@@ -55,8 +55,8 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   cursorX : 0,
 
   /**
-  Initializes the table of content and handles options
-  */
+   Initializes the table of content and handles options
+   */
   init : function(annotations){
     var _this = this;
     // init
@@ -116,58 +116,58 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   },
 
   /**
-  * Creates a timeline slider to seek within the playback time
-  */
+   * Creates a timeline slider to seek within the playback time
+   */
   createTimelineControl : function() {
     var _this = this;
     //
     /*if (this.video.readyState) {
-      clearInterval(this.interval);
-      clearInterval(this.interval);*/
-      //var video_duration = _this.video.duration; //$(this.options.selector).attr('duration');
-      this.video_seek.slider({
-        value: 0,
-        step: 0.01,
-        orientation: 'horizontal',
-        range: 'min',
-        max: vi2.observer.player.duration(),
-        animate: false,
-        slide: function(event, ui) {
-            _this.seeksliding = true;
-        },
-        start: function(event, ui) {
-          vi2.observer.log({context:'player',action:'seek-start',values: [ui.value]} );
-          _this.buffclick++;
-          _this.seeksliding = true;
-        },
-        stop: function(event, ui) {
-          vi2.observer.log({context:'player',action:'seek-stop',values:[ui.value]}	);
-          _this.seeksliding = false;
-          //if(_this.percentLoaded > (ui.value / _this.duration())){
-          vi2.observer.player.currentTime( parseFloat(Math.ceil(ui.value)) ); // XXX bugy / webkit fix
-        }
+     clearInterval(this.interval);
+     clearInterval(this.interval);*/
+    //var video_duration = _this.video.duration; //$(this.options.selector).attr('duration');
+    this.video_seek.slider({
+      value: 0,
+      step: 0.01,
+      orientation: 'horizontal',
+      range: 'min',
+      max: vi2.observer.player.duration(),
+      animate: false,
+      slide: function(event, ui) {
+        _this.seeksliding = true;
+      },
+      start: function(event, ui) {
+        vi2.observer.log({context:'player',action:'seek-start',values: [ui.value]} );
+        _this.buffclick++;
+        _this.seeksliding = true;
+      },
+      stop: function(event, ui) {
+        vi2.observer.log({context:'player',action:'seek-stop',values:[ui.value]}	);
+        _this.seeksliding = false;
+        //if(_this.percentLoaded > (ui.value / _this.duration())){
+        vi2.observer.player.currentTime( parseFloat(Math.ceil(ui.value)) ); // XXX bugy / webkit fix
+      }
 
-      });
+    });
 
     /*} else {
-      // try reinitiate the slider as long the ...?
-      this.interval = setInterval(function() { _this.createTimelineControl(); }, 150);
-    }*/
+     // try reinitiate the slider as long the ...?
+     this.interval = setInterval(function() { _this.createTimelineControl(); }, 150);
+     }*/
 
   },
 
   /*
-  * Add Video preview on timeline
-  * todo: event could be logged
-  **/
+   * Add Video preview on timeline
+   * todo: event could be logged
+   **/
   createTimelineVideoPreview : function(){
     var
-        _this = this,
-        width = $( this.options.timelineSelector ).width(),
-        left = ($( this.options.timelineSelector )).offset().left,
-        t = 1,
-        o = null
-        ;
+      _this = this,
+      width = $( this.options.timelineSelector ).width(),
+      left = ($( this.options.timelineSelector )).offset().left,
+      t = 1,
+      o = null
+      ;
 
     var img = new Image();
     img.id = 'videopreview';//);//.attr('src', _this.options.previewPath + "001.jpg");
@@ -178,17 +178,17 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
 
     // event
     var handleTimelineMoves = function(event){
-        $( timeline_preview ).css({ left: (event.pageX - 100) });
-        t = (Math.round( ( event.pageX / width ) * vi2.observer.player.duration() ) - 20); // correction value is unclear
-        o = new Image();
-        img_selector = document.getElementById("videopreview");
-        listener = function(event2){
-          img_selector.src = o.src;
-        };
-        o.removeEventListener('load', listener, false);
-        o.addEventListener('load', listener, false);
-        o.src = _this.options.previewPath + "" + t + ".jpg";//  + "?_=" + (+new Date());
-        o.onerror = function () { this.style.display = "none"; };
+      $( timeline_preview ).css({ left: (event.pageX - 100) });
+      t = (Math.round( ( event.pageX / width ) * vi2.observer.player.duration() ) - 20); // correction value is unclear
+      o = new Image();
+      img_selector = document.getElementById("videopreview");
+      listener = function(event2){
+        img_selector.src = o.src;
+      };
+      o.removeEventListener('load', listener, false);
+      o.addEventListener('load', listener, false);
+      o.src = _this.options.previewPath + "" + t + ".jpg";//  + "?_=" + (+new Date());
+      o.onerror = function () { this.style.display = "none"; };
     };
     var el = document.getElementsByClassName( 'vi2-timeline-main' );
     el[0].removeEventListener('mousemove', handleTimelineMoves );
@@ -196,17 +196,17 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   },
 
   /*
-  * Add Video preview on timeline
-  * todo: event could be logged
-  **/
+   * Add Video preview on timeline
+   * todo: event could be logged
+   **/
   createTimelineSlidePreview : function(){
     var
-        _this = this,
-        width = $( this.options.timelineSelector ).width(),
-        left = $( this.options.timelineSelector ).offset().left,
-        t = 1,
-        o = null
-        ;
+      _this = this,
+      width = $( this.options.timelineSelector ).width(),
+      left = $( this.options.timelineSelector ).offset().left,
+      t = 1,
+      o = null
+      ;
     var timeline_preview = $('<div></div>')
       .addClass('vi2-timeline-preview')
       .appendTo( this.options.timelineSelector );
@@ -214,25 +214,25 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
     // event
     var el = document.getElementsByClassName( 'vi2-timeline-main' );
     el[0].addEventListener('mousemove', function(event){
-        $( timeline_preview ).css({ left: (event.pageX - 100) });
-        t = (Math.floor( ( vi2.observer.player.duration() / width ) * Math.floor(event.pageX) )).toString();
+      $( timeline_preview ).css({ left: (event.pageX - 100) });
+      t = (Math.floor( ( vi2.observer.player.duration() / width ) * Math.floor(event.pageX) )).toString();
 
-        o = new Image();
-        o.src = _this.options.previewPath + "" + t + ".png";
-        listener = function(event2){
-          $( timeline_preview )
-            //.css({ left: (event.pageX - 100) })
-            .html(o);
-        };
-        o.removeEventListener('load', listener, true);
-        o.addEventListener('load', listener, true);
+      o = new Image();
+      o.src = _this.options.previewPath + "" + t + ".png";
+      listener = function(event2){
+        $( timeline_preview )
+        //.css({ left: (event.pageX - 100) })
+          .html(o);
+      };
+      o.removeEventListener('load', listener, true);
+      o.addEventListener('load', listener, true);
     }, false);
   },
 
   /**
-  * Displays markers on the timeline
-  * options: hasTooltip, clickable, type,
-  */
+   * Displays markers on the timeline
+   * options: hasTooltip, clickable, type,
+   */
   addTimelineMarkers : function(type, data, timelineSelector){
     var _this= this;
     if( timelineSelector === undefined ){
@@ -250,8 +250,8 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
       if (isNaN(progress) || progress > $( timelineSelector ).width()) { return;}
 
       var sp = $('<a></a>')			// $('<span></span>');
-        .addClass( type + '-timeline-marker ttoc')
-        .attr('style','left:'+ progress +'px;')
+          .addClass( type + '-timeline-marker ttoc')
+          .attr('style','left:'+ progress +'px;')
         ;
       if( _this.options[ type ].markerHasTooltip){
         sp
@@ -260,9 +260,9 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
       }
       if( _this.options[ type ].markerIsClickable ){
         sp.bind('click', function(event){
-            vi2.observer.player.currentTime( val.occ[0] );
-            // xxx bug for type === assessment !!! todo
-            vi2.observer.log( {context:type, action:'timeline-link-click',values:[val.name,val.author,val.occ[0]]});
+          vi2.observer.player.currentTime( val.occ[0] );
+          // xxx bug for type === assessment !!! todo
+          vi2.observer.log( {context:type, action:'timeline-link-click',values:[val.name,val.author,val.occ[0]]});
         });
       }
       timeline.append(sp); // val.title
@@ -270,34 +270,34 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   },
 
   /*
-  * Event is called when the total playback time has been determined
-  **/
+   * Event is called when the total playback time has been determined
+   **/
   handleDurationChange : function(e) {
     this.createTimelineControl();
     //if( $(_this.options.selector).attr('duration') != undefined )
 
-      vi2.observer.player.currentTime( this.seek );
-      $(vi2.observer).trigger('player.ready', [vi2.observer.player.seqList[vi2.observer.player.seqNum].id, vi2.observer.player.seqNum]);
+    vi2.observer.player.currentTime( this.seek );
+    $(vi2.observer).trigger('player.ready', [vi2.observer.player.seqList[vi2.observer.player.seqNum].id, vi2.observer.player.seqNum]);
 
 
     /*if (Number(this.seek) > 0) {
-      if(this.percentLoaded > (this.seek / this.duration())){
-        //this.currentTime(seek); // bugy in production use or on remote sites
-        vi2.observer.player.currentTime( this.seek );
-        $(vi2.observer).trigger('player.ready', [vi2.observer.player.seqList[vi2.observer.player.seqNum]['id'], vi2.observer.player.seqNum]);
-      }
-    }*/
+     if(this.percentLoaded > (this.seek / this.duration())){
+     //this.currentTime(seek); // bugy in production use or on remote sites
+     vi2.observer.player.currentTime( this.seek );
+     $(vi2.observer).trigger('player.ready', [vi2.observer.player.seqList[vi2.observer.player.seqNum]['id'], vi2.observer.player.seqNum]);
+     }
+     }*/
 
   },
 
   /*
-  * Event is called during the videos is buffering
-  **/
+   * Event is called during the videos is buffering
+   **/
   handleProgress : function (e) {
     //_this.setProgressRail(e);
     var
       target = this.video;//(e != undefined) ? e.target : this.video,
-      percent = null;
+    percent = null;
 
     if (target && target.buffered && target.buffered.length > 0 && target.buffered.end && target.duration) {
       percent = target.buffered.end(0) / target.duration;
@@ -319,8 +319,8 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   },
 
   /*
-  * Event is called every time when the playback time changes
-  **/
+   * Event is called every time when the playback time changes
+   **/
   handleTimeupdate : function(e) {
     if (!this.seeksliding) {
       this.video_seek.slider('value', vi2.observer.player.currentTime() ); // / vi2.observer.player.duration()
@@ -329,44 +329,64 @@ Vi2.AnnotatedTimeline = $.inherit(/** @lends Vi2.TableOfContents# */{ //
   },
 
   /**/
-	highlighTimeline : function() {
-		var timeline = document.querySelector('.vi2-timeline-main');
+  highlightTimeline : function(videoElement, timelineSelector, firstLoopPoint, secondLoopPoint, countLoopTimelinePoints) {
+    if (timelineSelector === undefined) {
+      timelineSelector = this.options.timelineSelector;
+    }
+    var videoDuration = videoElement.duration;
+    var loopMarker = document.createElement('span');
+    loopMarker.classList.add('vi2-timeline-loop-marker');
 
-		// console.log(timeline);
-	},
+    if (countLoopTimelinePoints === 0) {
+      timelineSelector.appendChild(loopMarker);
+      loopMarker.style.left = (firstLoopPoint/videoDuration * 100) + '%';
+
+    } else if (countLoopTimelinePoints === 1) {
+      var firstCreatedLoopMarker = timelineSelector.querySelector('.vi2-timeline-loop-marker');
+      firstCreatedLoopMarker.parentNode.removeChild(firstCreatedLoopMarker);
+      timelineSelector.appendChild(loopMarker);
+      loopMarker.style.left = (firstLoopPoint/videoDuration * 100) + '%';
+      loopMarker.style.width = (secondLoopPoint/videoDuration * 100) - (firstLoopPoint/videoDuration * 100) + '%';
+
+    }
+  },
 
   /**/
-	deleteHighlightTimeline :  function() {
-		var timeline = document.querySelector('.vi2-timeline-main');
+  deleteHighlightTimeline :  function(timelineSelector) {
+    if(timelineSelector === undefined) {
+      timelineSelector = this.options.timelineSelector;
+    }
+    var loopMarkersArray = timelineSelector.querySelectorAll('.vi2-timeline-loop-marker');
 
-		// console.log(timeline);
-	},
-		
-		
+    for (var i = 0; i < loopMarkersArray.length; i++) {
+      loopMarkersArray[i].parentNode.removeChild(loopMarkersArray[i]);
+    }
+  },
+
   /**/
   pixelInTime : function(){}
-		
-		
-	
-		
+
+
+
+
   /* --
-  setCurrentRail: function(e) {
+   setCurrentRail: function(e) {
 
-    var t = this;
+   var t = this;
 
-    if (t.media.currentTime != undefined && t.media.duration) {
+   if (t.media.currentTime != undefined && t.media.duration) {
 
-      // update bar and handle
-      if (t.total && t.handle) {
-        var
-          newWidth = t.total.width() * t.media.currentTime / t.media.duration,
-          handlePos = newWidth - (t.handle.outerWidth(true) / 2);
+   // update bar and handle
+   if (t.total && t.handle) {
+   var
+   newWidth = t.total.width() * t.media.currentTime / t.media.duration,
+   handlePos = newWidth - (t.handle.outerWidth(true) / 2);
 
-        t.current.width(newWidth);
-        t.handle.css('left', handlePos);
-      }
-    }
+   t.current.width(newWidth);
+   t.handle.css('left', handlePos);
+   }
+   }
 
-  }	*/
+   }	*/
 
 }); // end class
